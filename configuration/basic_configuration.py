b7639f024a6b8c54df6c0ea4df5a4abec1fd2535
sagarafr <sagarafr@gmail.com>
2017-05-20 20:15:12 +0200

    feat(config): Have a new config field

--- configuration/basic_configuration.py
+++ configuration/basic_configuration.py
@@ -7,6 +7,7 @@ class BasicConfiguration(object):
         with open(file, 'r') as fd_file:
             self._parser.read_file(fd_file)
             self._admin = None if not self._parser.has_option('DEFAULT', 'admins') else [int(e) for e in self._parser['DEFAULT']['admins'].split(';')]
+            self._insult_file = None if not self._parser.has_option('DEFAULT', 'insult_file') else str(self._parser['DEFAULT']['insult_file'])
 
     @property
     def token(self):
@@ -15,3 +16,7 @@ class BasicConfiguration(object):
     @property
     def admins(self):
         return self._admin
+
+    @property
+    def insult_file(self):
+        return self._insult_file
